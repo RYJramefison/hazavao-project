f5fbbd1314ae42a0e35f5faf77725b749297b867
juninho <hei.juninho.2@gmail.com>
2025-06-13 08:45:50 +0300

    feat: add service OpenAIService

---
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HazavaoController.java
@@ -0,0 +1,20 @@
+package com.my.company.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@AllArgsConstructor
+public class HazavaoController {
+
+    private final OpenAIService openAIService;
+
+
+    @GetMapping("/hazavao")
+    public ResponseEntity<String> getDefinition(@RequestParam String teny) {
+        String definition = openAIService.getDefinitionInMalagasy(teny);
+        return ResponseEntity.ok(definition);
+    }
+}
